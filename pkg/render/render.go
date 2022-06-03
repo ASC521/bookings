aa5aad26a04e4c2e61b76f4f14b4cd3bd9e5d641
Alex Curto <curto.alexander@gmail.com>
2022-06-03 18:42:24 -0400

    Initial commit

---
+++ pkg/render/render.go
@@ -0,0 +1,94 @@
+package render
+
+import (
+	"bytes"
+	"fmt"
+	"github.com/asc521/bookings/pkg/config"
+	"github.com/asc521/bookings/pkg/models"
+	"html/template"
+	"log"
+	"net/http"
+	"path/filepath"
+)
+
+var functions = template.FuncMap{}
+var app *config.AppConfig
+
+// NewTemplates sets the config for the template package
+func NewTemplates(a *config.AppConfig) {
+	app = a
+}
+
+func AddDefaultData(td *models.TemplateData) *models.TemplateData {
+	return td
+}
+
+func RenderTemplate(w http.ResponseWriter, tmpl string, td *models.TemplateData) {
+
+	var tc map[string]*template.Template
+	if app.UseCache {
+		tc = app.TemplateCache
+	} else {
+		tc, _ = CreateTemplateCache()
+	}
+
+	t, ok := tc[tmpl]
+	if !ok {
+		log.Fatal("Could not get template from template cache")
+	}
+
+	buf := new(bytes.Buffer)
+
+	td = AddDefaultData(td)
+	_ = t.Execute(w, td)
+
+	/*
+		Why using a buffer?
+
+		Instructor Response:
+		I write to the buffer, instead of straight to the ResponseWriter because I can check to see if there's an error,
+		and determine where it comes from more easily.
+
+		If you write straight to the io.Writer (ResponseWriter), you can still check for an error, but have to do some extra
+		work to see where it comes from.
+
+		That's it.
+	*/
+	_, err := buf.WriteTo(w)
+	if err != nil {
+		fmt.Println("Error writing template to browser", err)
+	}
+}
+
+// CreateTemplateCache creates a template cache as a map
+func CreateTemplateCache() (map[string]*template.Template, error) {
+	myCache := map[string]*template.Template{}
+
+	pages, err := filepath.Glob("./templates/*.page.gohtml")
+	if err != nil {
+		return myCache, err
+	}
+	for _, page := range pages {
+		name := filepath.Base(page)
+
+		ts, err := template.New(name).Funcs(functions).ParseFiles(page)
+		if err != nil {
+			return myCache, err
+		}
+
+		matches, err := filepath.Glob("./templates/*.layout.gohtml")
+		if err != nil {
+			return myCache, err
+		}
+
+		if len(matches) > 0 {
+			ts, err = ts.ParseGlob("./templates/*.layout.gohtml")
+			if err != nil {
+				return myCache, err
+			}
+		}
+
+		myCache[name] = ts
+	}
+	return myCache, nil
+}
